cc56cde4d23b02ad74cea7eaf69709e2bae46e59
Oleh Bendryk <olegbendrik@gmail.com>
2021-11-20 14:34:12 +0200

    add-groups CRUD

---
+++ database/migrations/2021_11_09_104321_create_customers_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers_groups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('group_id');
+            $table->unsignedInteger('customer_id');
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers_groups');
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,37 +1,6 @@
 @extends('master')
 
 @section('content')
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-            <i class="fas fa-users-cog"> SMS-Marketing</i>
-            <p class="fs-6 text-muted">Dashboard</p>
-        </a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-                <a class="nav-link px-3" href="">Sign out</a>
-            </div>
-        </div>
-    </header>
 
-    <div class="container-fluid">
-        <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div class="position-sticky pt-3">
-                    <ul class="nav flex-column">
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('customer.index') }}">
-                                <i class="fas fa-user-shield"></i>
-                                Customers
-                            </a>
-                        </li>
-                      </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
 @endsection
 
